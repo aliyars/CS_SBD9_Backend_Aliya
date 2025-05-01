2caa655fbb525ff3f855c8b2dbd0f9f43b410669
aliyars <losingyousuckss@gmail.com>
2025-05-01 15:36:05 +0700

    backend

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const corsOptions = {
+  origin: [process.env.FRONTEND_URL, "localhost:3000"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use(`/user`, require(`./src/routes/user.route`));
 app.use(`/store`, require(`./src/routes/storeRoutes`));
